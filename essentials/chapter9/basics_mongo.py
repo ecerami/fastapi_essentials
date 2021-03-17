ab538d0ad4072d73cfc3b4a628eaa2cf7c76ab21
Ethan Cerami <cerami@gmail.com>
2021-03-17 11:41:58 -0400

    Added

---
+++ essentials/chapter9/basics_mongo.py
@@ -0,0 +1,40 @@
+# Illustrates the basics of using PyMongo
+from pymongo import MongoClient
+import pprint
+
+# Pretty Print Config
+pp = pprint.PrettyPrinter(indent=4)
+
+# Connect to the default, local MongoDB server
+client = MongoClient()
+
+# Connect to the Slack Database
+db = client["slack"]
+msg_collection = db["messages"]
+
+# Create a message dict
+message = {
+    "channel": "dev",
+    "author": "cerami",
+    "text": "Hello, world!"
+}
+
+# Insert One Record and Show the Inserted ID
+# To insert multiple records at once, use insert_many()
+result = msg_collection.insert_one(message)
+print("Inserted:  %s" % result.inserted_id)
+
+# Now get all documents back
+for doc in msg_collection.find():
+    pp.pprint(doc)
+
+# Or get all documents in one channel
+for doc in msg_collection.find({"channel": "dev"}):
+    pp.pprint(doc)
+
+# Or get all documents by specific author
+for doc in msg_collection.find({"author": "cerami"}):
+    pp.pprint(doc)
+
+# Close connection
+client.close()
