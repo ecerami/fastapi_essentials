c2beb3cda29851274f8bf3324674f0b2b785cf61
Ethan Cerami <cerami@gmail.com>
2021-03-26 09:36:36 -0400

    Updated to latest benchmark code

---
+++ essentials/mongo_benchmark/mongo1.py
@@ -0,0 +1,31 @@
+# FastAPI + MongoDB
+# Option 1: Use Context Managers via the with statement.
+from pymongo import MongoClient
+from fastapi import FastAPI
+from typing import List
+import os
+
+# Sample Movie Database from Atlas MongoDB
+DB = "sample_mflix"
+MSG_COLLECTION = "movies"
+
+# Instantiate the FastAPI
+app = FastAPI()
+
+uri = "mongodb+srv://%s:%s@%s/sample_mflix?retryWrites=true&w=majority" % (
+    os.environ["MONGO_USER"],
+    os.environ["MONGO_PASSWORD"],
+    os.environ["MONGO_HOST"],
+)
+
+
+@app.get("/movies/{genre}", response_model=List[str])
+def get_movies(genre: str):
+    """Get first N movies in the specified genre."""
+    with MongoClient(uri) as client:
+        movie_collection = client[DB][MSG_COLLECTION]
+        msg_list = movie_collection.find({"genres": genre}).limit(100)
+        movie_title_list = []
+        for msg in msg_list:
+            movie_title_list.append(msg["title"])
+        return movie_title_list
